03a602f34eb519ff7a0e01d836bafcb667bf6bae
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-07-05 02:20:50 +1000

    main

---
+++ src/components/NavbarBS.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
+
+export default class NavbarBS extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" variant={"dark"} expand="lg">
+                    <Container fluid>
+                        <Navbar.Brand href="#">Online Shopping</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <Nav.Link href="#action2">MEN</Nav.Link>
+                                <Nav.Link href="#action3">WOMEN</Nav.Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
