d4ce95f62c519c1c5596d68480cf12e35b3f58c3
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-07-04 15:18:53 +1000

    main

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactfffff
+          Learn React
         </a>
       </header>
     </div>
